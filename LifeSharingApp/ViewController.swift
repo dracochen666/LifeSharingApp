6ac9b6bcea8d744ce4360d602fde79b627dfffbc
chenlong <2474184221@qq.com>
2023-03-17 15:35:40 +0800

    first commit

---
+++ LifeSharingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeSharingApp
+//
+//  Created by 陈龙 on 2023/3/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
