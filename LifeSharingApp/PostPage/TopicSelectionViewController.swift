8d17e168bd04939644d052930e2190dd1bede9ba
chenlong <2474184221@qq.com>
2023-04-04 18:28:48 +0800

    添加：在话题搜索页面添加话题选择ViewPager

--- LifeSharingApp/PostPage/TopicSelectionViewController.swift
+++ LifeSharingApp/PostPage/TopicSelectionViewController.swift
@@ -30,16 +30,38 @@ class TopicSelectionViewController: UIViewController {
         
         return textField
     }()
+    lazy var viewPager: ViewPager = {
+        let vp = ViewPager(sizeConfiguration: .fillEqually(height: 60))
+        return vp
+    }()
 
     func setupUI() {
+        let tabItems = [TabbedItem(title: "美食"),
+                        TabbedItem(title: "旅游"),
+                        TabbedItem(title: "风景"),
+                        TabbedItem(title: "摄影"),
+                        TabbedItem(title: "汽车")]
+
+        let view1 = UIView()
+        view1.layer.cornerRadius = kGlobalCornerRadius
+        let view2 = UIView()
+        let view3 = UIView()
+        let view4 = UIView()
+        let view5 = UIView()
         
+        viewPager.pagedView.pages = [view1, view2, view3, view4, view5]
+        viewPager.tabbedView.tabs = tabItems
         self.view.backgroundColor = UIColor(named: kSecondLevelColor)
         self.view.addSubview(searchTextField)
+        self.view.addSubview(viewPager)
         
         //约束
         searchTextField.horizontalAnchors == self.view.horizontalAnchors + 20
         searchTextField.topAnchor == self.view.topAnchor + kCustomGlobalMargin
         searchTextField.heightAnchor == 40
         
+        viewPager.horizontalAnchors == self.view.horizontalAnchors + 20
+        viewPager.topAnchor == searchTextField.bottomAnchor + kCustomGlobalMargin
+        viewPager.bottomAnchor == self.view.bottomAnchor
     }
 }
